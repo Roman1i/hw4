8ea260e37817390603ec516138b1580365bb33b6
Roman <igoshinr.estho@gmail.com>
2022-08-08 02:56:15 +0300

    массивы

---
+++ blueberry/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = new Random().Next(3, 1001);
+int[] arr = new int[n];
+for ( int i = 0; i < n; i++)  arr[i] = new Random().Next(1,1001);
+int sum = 0;
+int max = 0;
+for (int count = 1; count < n-1; count++) 
+{
+    sum = arr[count-1]+arr[count]+arr[count+1];
+    if (sum > max) max = sum; 
+}
+Console.WriteLine(max);
